f306fbfddb8e49e8325eb6d8400221b66a607c92
Zack Youngren <zack@youngren.io>
2023-01-24 17:37:13 -0500

    Socket mode

--- src/app.js
+++ src/app.js
@@ -13,9 +13,11 @@ const { App } = slackBolt;
 
 // Initializes your app with your bot token and signing secret
 const app = new App({
-  token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  appToken: process.env.SOCKET_TOKEN,
   ignoreSelf: false,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  socketMode: true,
+  token: process.env.SLACK_BOT_TOKEN,
 });
 
 // Messages that start with ? are interpreted as commands
