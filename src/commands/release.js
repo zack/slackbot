e2ab1deff60df9f7309317e738f1949d860ae98b
Zack Youngren <zack@youngren.io>
2023-01-24 11:41:49 -0500

    Add commit title to ?release feedback

--- src/commands/release.js
+++ src/commands/release.js
@@ -22,14 +22,22 @@ const release = async ({ body, say }) => {
       out = 'Something went wrong. I hope you have ssh access.';
       respondThreaded(say, body, out);
     } else {
-      const revision = childProcess
+      // It seems like pm2 restarts the process before this can return if there
+      // is in fact a release, which means the user won't get feedback that a
+      // release happened.. I don't know what to do about that.
+
+      const SHA = childProcess
         .execSync('git rev-parse HEAD')
         .toString().trim();
 
+      const title = childProcess
+        .execSync('git show-branch --no-name HEAD')
+        .toString().trim();
+
       if (response.summary.changes > 0) {
-        out = `Successfully deployed ${revision}`;
+        out = `Successfully deployed ${SHA}`;
       } else {
-        out = `No changes detected. Already on latest commit: ${revision}`;
+        out = `No changes deteced. Already on latest commit: ${SHA} (${title})`;
       }
       respondThreaded(say, body, out);
     }
