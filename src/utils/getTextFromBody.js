522a6b979269a1250abae24596b33d7eb8454574
Zack Youngren <zack@youngren.io>
2023-01-24 14:13:09 -0500

    Fix bug in getTextFromBody that prevented it from workign in threads

--- src/utils/getTextFromBody.js
+++ src/utils/getTextFromBody.js
@@ -2,14 +2,14 @@ const getTextFromBody = async (app, body) => {
   const messageTs = body.event.item.ts;
   const { channel } = body.event.item;
 
-  const history = await app.client.conversations.history({
+  const replies = await app.client.conversations.replies({
     channel,
     inclusive: true,
-    oldest: messageTs,
+    ts: messageTs,
     limit: 1,
   });
 
-  return (history.messages[0].text);
+  return (replies.messages[0].text);
 };
 
 export default getTextFromBody;
