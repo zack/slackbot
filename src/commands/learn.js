37824a8d008c833f66185650517e0af0266ea036
Zack Youngren <zack@youngren.io>
2023-01-24 12:15:12 -0500

    Simplify all the learns code

--- src/commands/learn.js
+++ src/commands/learn.js
@@ -1,12 +1,8 @@
 import Database from 'better-sqlite3';
 
-import cleanUser from '../utils/cleanUser.js';
 import getTextFromBody from '../utils/getTextFromBody.js';
 import sample from '../utils/sample.js';
-import verifyUser from '../utils/verifyUser.js';
 import { respond, respondThreaded } from '../utils/respond.js';
-
-const RE_USER = /^<@.*>$/;
 
 const db = new Database('../learns.db');
 db.pragma('journal_mode = WAL'); // https://github.com/WiseLibs/better-sqlite3#usage
@@ -20,9 +16,7 @@ db.exec(`CREATE TABLE IF NOT EXISTS learns(
 const gimme = async ({
   body, say, text,
 }) => {
-  const learnee = (RE_USER.exec(text) !== null)
-    ? cleanUser(text)
-    : text;
+  const learnee = text;
 
   const learns = db.prepare('SELECT content, ts FROM learns WHERE learnee = ?').all(learnee);
 
@@ -43,52 +37,51 @@ const learn = async (app, body, content, learnee, learner, learnerName, say) => 
     try {
       db.prepare('INSERT INTO learns(learnee, learner, content) values (?, ?, ?)').run(learnee, learner, content);
 
-      if (await verifyUser(app, learnee)) {
-        out = `Learned about <@${learnee}> (by ${learnerName})!`;
-      } else {
-        out = `Learned about ${learnee} (by ${learnerName})!`;
-      }
-
-      respondThreaded(say, body, out);
+      out = `Learned about ${learnee} (by ${learnerName})!`;
     } catch {
-      respondThreaded(say, body, 'I already know that.');
+      out = 'I already know that.';
     }
   } else {
     out = 'No empty learns!';
   }
+
+  respondThreaded(say, body, out);
+};
+
+const getCommandData = async (app, body, text) => {
+  const args = text.split(' ');
+  const content = args.slice(1).join(' ');
+  const learnee = args[0];
+  const learner = body.event.user;
+  const learnerProfile = await app.client.users.info({ user: learner });
+  const learnerName = learnerProfile.user.profile.display_name;
+
+  return {
+    content,
+    learnee,
+    learner,
+    learnerName,
+  };
 };
 
 const learnCommand = async ({
   app, body, text, say,
 }) => {
-  const args = text.split(' ');
-  const learnee = (RE_USER.exec(args[0]) !== null)
-    ? cleanUser(args[0])
-    : args[0];
-  const learner = body.event.user;
-  const learnerProfile = await app.client.users.info({ user: learner });
-  const learnerName = learnerProfile.user.profile.display_name;
-
-  if (RE_USER.exec(text) && !(await verifyUser(app, learnee))) {
-    respondThreaded(say, body, "Sorry, I don't know who that is.");
-    return;
-  }
-
-  const content = args.slice(1).join(' ');
+  const {
+    content,
+    learnee,
+    learner,
+    learnerName,
+  } = await getCommandData(app, body, text);
 
   learn(app, body, content, learnee, learner, learnerName, say);
 };
 
 const unlearn = async (app, body, content, learnee, unlearnerName, say) => {
-  if (!(await verifyUser(app, learnee))) {
-    respondThreaded(say, body, "Sorry, I don't know who that is.");
-    return;
-  }
-
   const { changes } = db.prepare('DELETE FROM learns where learnee = ? and content = ?').run(learnee, content);
 
   const out = changes > 0
-    ? `Unlearned about <@${learnee}> (by ${unlearnerName})!`
+    ? `Unlearned about ${learnee} (by ${unlearnerName})!`
     : "I don't think I knew that. I certainly don't know it now.";
 
   respondThreaded(say, body, out);
@@ -97,31 +90,39 @@ const unlearn = async (app, body, content, learnee, unlearnerName, say) => {
 const unlearnCommand = async ({
   app, body, text, say,
 }) => {
-  const args = text.split(' ');
-  const learnee = cleanUser(args[0]);
-  const unlearner = body.event.user;
-  const unlearnerProfile = await app.client.users.info({ user: unlearner });
-  const unlearnerName = unlearnerProfile.user.profile.display_name;
+  const {
+    content,
+    learnee,
+    learnerName,
+  } = await getCommandData(app, body, text);
 
-  if (!(await verifyUser(app, learnee))) {
-    respondThreaded(say, body, "Sorry, I don't know who that is.");
-    return;
-  }
+  unlearn(app, body, content, learnee, learnerName, say);
+};
 
-  const content = args.slice(1).join(' ');
+const getEmojiData = async (app, body) => {
+  const content = await getTextFromBody(app, body);
+  const learnee = `<@${body.event.item_user}>`;
+  const learner = body.event.user;
+  const learnerProfile = await app.client.users.info({ user: learner });
+  const learnerName = learnerProfile.user.profile.display_name;
 
-  unlearn(app, body, content, learnee, unlearnerName, say);
+  return {
+    content,
+    learnee,
+    learner,
+    learnerName,
+  };
 };
 
 const learnEmoji = async ({
   app, body, say,
 }) => {
-  const learnee = body.event.item_user;
-  const learner = body.event.user;
-  const learnerProfile = await app.client.users.info({ user: learner });
-  const learnerName = learnerProfile.user.profile.display_name;
-
-  const content = await getTextFromBody(app, body);
+  const {
+    content,
+    learnee,
+    learner,
+    learnerName,
+  } = await getEmojiData(app, body);
 
   learn(app, body, content, learnee, learner, learnerName, say);
 };
@@ -129,14 +130,13 @@ const learnEmoji = async ({
 const unlearnEmoji = async ({
   app, body, say,
 }) => {
-  const learnee = body.event.item_user;
-  const unlearner = body.event.user;
-  const unlearnerProfile = await app.client.users.info({ user: unlearner });
-  const unlearnerName = unlearnerProfile.user.profile.display_name;
+  const {
+    content,
+    learnee,
+    learnerName,
+  } = await getEmojiData(app, body);
 
-  const content = await getTextFromBody(app, body);
-
-  unlearn(app, body, content, learnee, unlearnerName, say);
+  unlearn(app, body, content, learnee, learnerName, say);
 };
 
 export {
