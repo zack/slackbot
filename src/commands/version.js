59f8bdac3ee1f544fd67565563631607ac8d5b2d
Zack Youngren <zack@youngren.io>
2023-01-24 12:17:09 -0500

    Add verbosity to ?version

--- src/commands/version.js
+++ src/commands/version.js
@@ -4,11 +4,17 @@ import { respondThreaded } from '../utils/respond.js';
 
 // Respond with the SHA of the git commit at HEAD
 const version = ({ body, say }) => {
-  const revision = childProcess
+  const SHA = childProcess
     .execSync('git rev-parse HEAD')
     .toString().trim();
 
-  respondThreaded(say, body, revision);
+  const title = childProcess
+    .execSync('git show-branch --no-name HEAD')
+    .toString().trim();
+
+  const out = `Running commit ${SHA} (${title})`;
+
+  respondThreaded(say, body, out);
 };
 
 export default version;
