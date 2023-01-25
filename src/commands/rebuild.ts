7d6f1d9aa21dc9da6df362d13522256c45da3898
Zack Youngren <zack@youngren.io>
2023-01-25 16:58:50 -0500

    add up to date, audited 451 packages in 510ms
    115 packages are looking for funding
      run `npm fund` for details
    
    found 0 vulnerabilities to ?rebuild

--- src/commands/rebuild.ts
+++ src/commands/rebuild.ts
@@ -7,9 +7,15 @@ const rebuild = async ({ body, say }) => {
   respondThreaded(say, body, 'Rebuilding... (please wait)');
   // Give it time to respond before building
   setTimeout(() => {
-    const out = childProcess.execSync('npm run build').toString();
-    respondThreaded(say, body, 'Finished! Output:');
-    respondThreaded(say, body, out);
+    respondThreaded(say, body, 'Running `npm install`...');
+    const installOut = childProcess.execSync('npm install').toString();
+    respondThreaded(say, body, 'Finished installing! Output:');
+    respondThreaded(say, body, installOut);
+
+    respondThreaded(say, body, 'Running `npm run build`...');
+    const buildOut = childProcess.execSync('npm run build').toString();
+    respondThreaded(say, body, 'Finished building! Output:');
+    respondThreaded(say, body, buildOut);
   }, 1000);
 };
 
