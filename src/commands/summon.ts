21f8ad0542cafcbcb7ac02211f66b9fcb40daad7
Zack Youngren <zack@youngren.io>
2023-01-26 09:15:18 -0500

    ?summon will pick randomly from the first page of results

--- src/commands/summon.ts
+++ src/commands/summon.ts
@@ -2,19 +2,21 @@ import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-
 import ImageSearch from 'image-search-google';
 import { respond } from '../utils/respond';
 
+import sample from '../utils/sample';
+
 dotenv.config();
 
 const searchClient = new ImageSearch(
   process.env.SEARCH_ENGINE_ID,
   process.env.GOOGLE_SEARCH_API_KEY,
 );
-const OPTIONS = { page: 1 };
 
-const pong = async ({
+const summon = async ({
   body, text, say,
 }) => {
-  const response = await searchClient.search(text, OPTIONS);
-  respond(say, body, `<${response[0].url}|${text}>`);
+  const response = await searchClient.search(text);
+  const image = sample(response);
+  respond(say, body, `<${image.url}|${text}>`);
 };
 
-export default pong;
+export default summon;
