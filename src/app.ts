ad63a43d992610fbe94b7bf71c689fc766df6d98
Zack Youngren <zack@youngren.io>
2023-12-29 11:41:50 -0500

    fix quotes

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
-import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 
-import announceEmojiChange from "./emojiChanges";
-import incrementChannelName from "./channelIncrementer";
-import { createDataSource, DataSourceTypeOptions } from "./data-source";
-import { dispatchCommand, dispatchReaction } from "./commandDispatcher";
+import announceEmojiChange from './emojiChanges';
+import incrementChannelName from './channelIncrementer';
+import { createDataSource, DataSourceTypeOptions } from './data-source';
+import { dispatchCommand, dispatchReaction } from './commandDispatcher';
 
 dotenv.config();
 
@@ -11,17 +11,17 @@ dotenv.config();
 (async () => {
   try {
     const type: DataSourceTypeOptions =
-      process.env.ORM_TYPE === "mysql" ? "mysql" : "better-sqlite3";
+      process.env.ORM_TYPE === 'mysql' ? 'mysql' : 'better-sqlite3';
     const AppDataSource = createDataSource(type);
     await AppDataSource.initialize();
   } catch (error: unknown) {
-    const errMessage = error instanceof Error ? error.message : "Unknown error";
+    const errMessage = error instanceof Error ? error.message : 'Unknown error';
     throw new Error(`Failed to initialize database: ${errMessage}`);
   }
 })();
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { App } = require("@slack/bolt");
+const { App } = require('@slack/bolt');
 
 const app = new App({
   appToken: process.env.SOCKET_TOKEN,
@@ -47,11 +47,11 @@ app.message(/^\?([^ ].*)/, async ({ body, context, say }) => {
   dispatchCommand(app, body, context, say);
 });
 
-const incrementerWords = (process.env.WORDS || "")
-  .split(",")
-  .filter((w) => w !== "");
+const incrementerWords = (process.env.WORDS || '')
+  .split(',')
+  .filter((w) => w !== '');
 if (incrementerWords.length > 0) {
-  app.message(new RegExp(`(${incrementerWords.join("|")})`, "g"), async () => {
+  app.message(new RegExp(`(${incrementerWords.join('|')})`, 'g'), async () => {
     incrementChannelName(
       app,
       process.env.INCREMENTING_CHANNEL_ID,
@@ -60,17 +60,17 @@ if (incrementerWords.length > 0) {
   });
 }
 
-app.event("reaction_added", async ({ body, context, say }) => {
+app.event('reaction_added', async ({ body, context, say }) => {
   const { reaction } = body.event;
   dispatchReaction(app, body, context, reaction, say);
 });
 
-app.event("emoji_changed", async ({ body }) => {
+app.event('emoji_changed', async ({ body }) => {
   announceEmojiChange(app, body, process.env.EMOJI_CHANNEL_ID);
 });
 
 (async () => {
   await app.start(process.env.PORT || 3000);
   // eslint-disable-next-line no-console
-  console.log("⚡️ Bolt app is running!");
+  console.log('⚡️ Bolt app is running!');
 })();
